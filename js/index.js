cad0694a2da3277acdd0f5f2dd306a2edcfcf43e
hegongshan <hegongshan@qq.com>
2024-09-10 22:34:37 +0800

    perf: code refactor

---
+++ js/index.js
@@ -0,0 +1,449 @@
+/* 
+ * -----------------------------------------------------------------------------
+ *                                  全局变量
+ * -----------------------------------------------------------------------------
+ */
+// 分页变量
+let pagination = {
+    paperList: [],
+    pageList: [],
+    pageSize: 10,
+    pageSizeStr: "10",
+    navigatePages: 10
+};
+
+/* 
+ * -----------------------------------------------------------------------------
+ *                                  自定义函数
+ * -----------------------------------------------------------------------------
+ */
+function failHandler($loadingTips) {
+    let alertHtml = template.render($("#alertTemplate").html(), {
+        title: "提示信息",
+        message: "请求失败，请重新尝试！"
+    });
+    $("#alert").html(alertHtml);
+    $('#alert').modal();
+
+    // 当请求失败时，不要继续显示加载图片
+    $loadingTips.empty();
+}
+
+function updateVenue(currentSelector, previousVals, selectpickerExtendedOptions) {
+    let categoryVals = $("#category").val();
+    let typeVals = $("#type").val();
+    let rankVals = $("#rank").val();
+    let $venue = $("#venue");
+
+    // 如果三者均没有值，则禁用venue
+    if (categoryVals.length == 0 && typeVals.length == 0 && rankVals.length == 0) {
+        // 清空venue的值，避免影响搜索结果
+        if ($venue.val().length > 0) {
+            $venue.selectpicker("deselectAll");
+        }
+        $venue.prop("disabled", true);
+        $venue.selectpicker("destroy");
+        $venue.selectpicker(selectpickerExtendedOptions);
+        return;
+    }
+
+    // 如果该元素当前和之前均没有值，则无需更新venue
+    if ($(currentSelector).val().length == 0 && previousVals[currentSelector].length == 0) {
+        return;
+    }
+
+    // 更新上一次选中的值
+    previousVals[currentSelector] = $(currentSelector).val();
+
+    // 清空列表
+    $venue.prop("disabled", false);
+    $venue.empty();
+
+    // 重新填充列表
+    let venueList = [];
+    let categoryList = {};
+    if (categoryVals.length == 0) {
+        categoryList = CATEGORY_LIST;
+    } else {
+        // 将选择的分类转换为JSON对象
+        categoryVals.forEach(function (element, index) {
+            categoryList[element] = "";
+        });
+    }
+    // 根据分类过滤
+    for (let category in categoryList) {
+        for (let venueDBLPUrl in CATEGORY_LIST[category]) {
+            // 根据类型过滤
+            if (!isTypeMatch(venueDBLPUrl, typeVals)) {
+                continue;
+            }
+
+            // 根据级别过滤
+            if (!isRankMatch(venueDBLPUrl, rankVals)) {
+                continue;
+            }
+
+            venueList.push({
+                url: venueDBLPUrl,
+                name: CATEGORY_LIST[category][venueDBLPUrl].venue,
+                type: venueDBLPUrl.startsWith("journals") ? "期刊" : "会议",
+                rank: CATEGORY_LIST[category][venueDBLPUrl].rank
+            });
+        }
+    }
+
+    let venueHtml = template.render($("#venueTemplate").html(), {
+        venueList: venueList
+    });
+    $venue.html(venueHtml);
+    $venue.selectpicker("destroy");
+    $venue.selectpicker(selectpickerExtendedOptions);
+}
+
+function updatePaperList(currentPageStr = '1') {
+    let tips = template.render($("#responseTipsTemplate").html(), {
+        count: pagination.paperList.length,
+    });
+    $("#tips").html(tips);
+
+    // 如果没有匹配的论文，则直接返回
+    if (pagination.paperList.length == 0) {
+        return;
+    }
+
+    // currentPage从1开始，其索引为currentPage - 1
+    let currentPage = parseInt(currentPageStr);
+
+    // 显示全部的论文
+    if (pagination.pageSizeStr == "All") {
+        pagination.pageSize = pagination.paperList.length;
+    } else {
+        pagination.pageSize = parseInt(pagination.pageSizeStr);
+    }
+
+    // 计算总页数和页列表
+    let totalPage = Math.floor((pagination.paperList.length + pagination.pageSize - 1) / pagination.pageSize);
+    pagination.pageList = Array.from({ length: totalPage }, (_, index) => index + 1);
+
+    // 计算开始论文索引和结束论文索引
+    let startPaperIndex = (currentPage - 1) * pagination.pageSize;
+    let endPaperIndex = Math.min(currentPage * pagination.pageSize, pagination.paperList.length);
+
+    // 计算开始页码和结束页码
+    let startPageIndex;
+    if (currentPage % pagination.navigatePages == 0) {
+        startPageIndex = parseInt((currentPage - 1) / pagination.navigatePages) * pagination.navigatePages;
+    } else {
+        startPageIndex = Math.floor((currentPage - 1) / pagination.navigatePages) * pagination.navigatePages;
+    }
+    let endPageIndex = Math.min(startPageIndex + pagination.navigatePages, pagination.pageList.length);
+
+    // 渲染分页代码
+    let paperHtml = template.render($("#paperTemplate").html(), {
+        paperList: pagination.paperList.slice(startPaperIndex, endPaperIndex),
+        startPaperNumber: startPaperIndex + 1,
+
+        pageInfo: {
+            firstPage: 1,
+            lastPage: totalPage,
+            pageList: pagination.pageList.slice(startPageIndex, endPageIndex),
+
+            previousPage: currentPage - 1,
+            currentPage: currentPage,
+            nextPage: currentPage + 1,
+
+            pageSizeList: ["10", "20", "50", "100", "All"],
+            pageSizeStr: pagination.pageSizeStr // 处理All
+        }
+    });
+    $("#result").html(paperHtml);
+}
+
+function fillPaperList() {
+    // 重置pageSize，避免影响不同查询
+    pagination.pageSizeStr = "10";
+
+    let invalidClass = "is-invalid";
+    let invalid = false;
+
+    // 验证关键词
+    let $searchFeedback = $(".search-feedback");
+    $(".q").each(function (index) {
+        let $input = $(this);
+
+        let query = $input.val().trim();
+        let isEmpty = query == "";
+        let hasChineseCharacter = query.match(/[\u4E00-\u9FA5]|[\uF900-\uFA2D]/);
+
+        if (isEmpty || hasChineseCharacter) {
+            invalid = true;
+            $input.addClass(invalidClass);
+
+            let errorMsg = isEmpty ? "关键词不能为空！" : "DBLP不支持中文关键词！";
+            $feedback = $searchFeedback.eq(index);
+            $feedback.text(errorMsg);
+        } else {
+            $input.removeClass(invalidClass);
+        }
+    });
+
+    // 验证起止年份
+    let $startYear = $("#startYear");
+    let $endYear = $("#endYear");
+    let startYearStr = $startYear.val().trim();
+    let endYearStr = $endYear.val().trim();
+    let isStartYearEmpty = isEmpty(startYearStr);
+    let isEndYearEmpty = isEmpty(endYearStr);
+
+    function handleYear($year, index) {
+        let currentYear = new Date().getFullYear()
+        let errorMsg = `年份必须位于 (0, ${currentYear}] 区间内！`;
+        let yearStr = $year.val().trim();
+        let yearVal = parseInt(yearStr);
+        let isYearInvalid = !isEmpty(yearStr) && (yearVal <= 0 || yearVal > currentYear);
+        let $yearFeedback = $(".year-feedback").eq(index);
+
+        if (isYearInvalid) {
+            $year.addClass(invalidClass);
+            $yearFeedback.text(errorMsg);
+        } else {
+            $year.removeClass(invalidClass);
+            $yearFeedback.empty();
+        }
+
+        return isYearInvalid;
+    }
+
+    if (!isStartYearEmpty || !isEndYearEmpty) {
+        let isStartYearInvalid = handleYear($startYear, 0);
+        let isEndYearInvalid = handleYear($endYear, 1);
+        if (isStartYearInvalid || isEndYearInvalid) {
+            invalid = true;
+        } else if (!isStartYearEmpty && !isEndYearEmpty
+            && parseInt(startYearStr) > parseInt(endYearStr)) {
+            $startYear.addClass(invalidClass);
+            $endYear.addClass(invalidClass);
+            $(".year-feedback").eq(0).text("开始年份必须≤结束年份！");
+
+            invalid = true;
+        }
+    } else {
+        $startYear.removeClass(invalidClass);
+        $endYear.removeClass(invalidClass);
+        $(".year-feedback").each(function (index) {
+            $(this).empty();
+        });
+    }
+
+    // 如果关键词和起止年份存在问题，则提前返回
+    if (invalid) {
+        return;
+    }
+
+    // 拼接查询字符串
+    let query = "";
+    $(".q").each(function (index) {
+        let val = $(this).val().trim();
+        if (index == 0) {
+            query += val;
+        } else {
+            let condition = $(".condition").eq(index - 1).val();
+            switch (condition) {
+                case "and":
+                    query += " " + val;
+                    break;
+                case "or":
+                    query += "|" + val;
+                    break;
+            }
+        }
+    });
+    // 判断是否需要显示查询字符串
+    if ($("#queryStringInput").prop("checked")) {
+        $("#queryString").text(query);
+    } else {
+        $("#queryString").empty();
+    }
+
+    let condition = {
+        categoryList: $("#category").val(),
+        typeList: $("#type").val(),
+        rankList: $("#rank").val(),
+        venueList: $("#venue").val(),
+        year: {
+            start: startYearStr,
+            end: endYearStr
+        }
+    };
+
+    // 搜索并更新论文列表
+    queryPaper(
+        query,
+        condition,
+        function (paperList) {
+            pagination.paperList = paperList;
+            updatePaperList();
+        },
+        function () {
+            failHandler($("#tips"));
+        }
+    );
+}
+
+function fillAbstractHandler($abstractTag, $loadingTips, abstract) {
+    if (isEmpty(abstract)) {
+        abstract = "没有找到摘要=_=";
+    }
+
+    // 填充摘要
+    $abstractTag.html(abstract);
+    $loadingTips.empty();
+}
+
+function fillAbstract(paperDOI, paperTitle = null, abstractSelector) {
+    let $abstractTag = $(abstractSelector);
+
+    // 读取缓存
+    if ($abstractTag.html()) {
+        $abstractTag.toggle();
+        return;
+    }
+
+    // init
+    let $loadingTips = $(abstractSelector + "Tips");
+    $loadingTips.html($("#loadingTipsTemplate").html());
+
+    // 查询摘要
+    queryAbstract(paperDOI, paperTitle, function (abstract) {
+        fillAbstractHandler($abstractTag, $loadingTips, abstract);
+    }, function () {
+        failHandler($loadingTips);
+    });
+}
+
+$(function () {
+    // 版权信息
+    let footerHtml = template.render($("#footerTemplate").html(), {
+        year: {
+            start: 2022,
+            current: new Date().getFullYear()
+        },
+    });
+    $("footer").html(footerHtml);
+
+    let $category = $("#category");
+    let $type = $("#type");
+    let $rank = $("#rank");
+    let $venue = $("#venue");
+
+    // 填充分类
+    let categoryList = [];
+    for (let venueDBLPUrl in CATEGORY_LIST) {
+        categoryList.push({ "name": venueDBLPUrl });
+    }
+    let categoryHtml = template.render($("#categoryTemplate").html(), {
+        categoryList: categoryList,
+    });
+    $category.html(categoryHtml);
+
+    // bootstrap-select初始化
+    let selectpickerOptions = {
+        actionsBox: true,
+        selectAllText: "全选",
+        deselectAllText: "取消全选",
+        noneResultsText: "未找到匹配的结果"
+    };
+    let selectpickerExtendedOptions = Object.assign({}, selectpickerOptions, {
+        liveSearch: true,
+        selectedTextFormat: "count > 3",
+        countSelectedText: "选择了{0}项"
+    });
+    $category.selectpicker(selectpickerExtendedOptions);
+    $type.selectpicker(selectpickerOptions);
+    $rank.selectpicker(selectpickerOptions);
+    $venue.prop("disabled", true);
+    $venue.selectpicker(selectpickerExtendedOptions);
+
+    var previousVals = {
+        "#category": [],
+        "#type": [],
+        "#rank": []
+    };
+    $category.on("hidden.bs.select", function () {
+        updateVenue("#category", previousVals, selectpickerExtendedOptions);
+    });
+    $type.on("hidden.bs.select", function () {
+        updateVenue("#type", previousVals, selectpickerExtendedOptions);
+    });
+    $rank.on("hidden.bs.select", function () {
+        updateVenue("#rank", previousVals, selectpickerExtendedOptions);
+    });
+
+    // 主题切换
+    $("#themeLight").click(function () {
+        $("body").removeClass("dark-theme");
+    });
+    $("#themeDark").click(function () {
+        $("body").addClass("dark-theme");
+    });
+
+    // 添加和删除新关键词
+    $("#addKeyword").click(function () {
+        $(".custom-switch").before($("#keywordTemplate").html());
+    });
+    $("body").on("click", ".remove-keyword", function () {
+        $(this).parent().parent().remove();
+    });
+
+    // 搜索
+    $("#search").click(function () {
+        fillPaperList();
+    });
+    $("#q").keydown(function (event) {
+        if (event.keyCode == 13) {
+            fillPaperList();
+        }
+    });
+
+    // 选择pageSize
+    $("body").on("change", "#pageSizeSelect", function () {
+        let oldPageSize = pagination.pageSize;
+
+        pagination.pageSizeStr = $(this).val();
+
+        // 避免不必要的页面更新操作
+        if (pagination.paperList.length <= oldPageSize
+            && (pagination.pageSizeStr == "All"
+                || pagination.paperList.length <= parseInt(pagination.pageSizeStr))) {
+            return;
+        }
+
+        updatePaperList();
+    });
+
+    // 常见问题
+    $("#faq").click(function () {
+        let faqHtml = template.render($("#alertTemplate").html(), {
+            title: "常见问题",
+            message: $("#faqTemplate").html()
+        });
+        let $alert = $("#alert");
+        $alert.html(faqHtml);
+        $alert.modal();
+    });
+
+    // 回到顶部
+    let $backToTopBtn = $("#backToTopBtn");
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $backToTopBtn.fadeIn();
+        } else {
+            $backToTopBtn.fadeOut();
+        }
+    });
+    $backToTopBtn.click(function () {
+        $("html, body").animate({ scrollTop: 0 }, 300);
+        return false;
+    });
+    $backToTopBtn.tooltip();
+});
--- js/search.js
+++ js/search.js
@@ -53,7 +53,7 @@ function isEmpty(str) {
     return !str || str.length === 0;
 }
 
-function doSearch(query, firstHit, pageSize, total, paperList, filter, fillPaperListCallback) {
+function doQueryPaper(query, firstHit, pageSize, total, paperList, condition, searchSuccessHandler, searchFailHandler) {
     let request_url = "https://dblp.uni-trier.de/search/publ/api?callback=?";
     let inputData = {
         q: query,
@@ -90,36 +90,36 @@ function doSearch(query, firstHit, pageSize, total, paperList, filter, fillPaper
                 // Whether the key or venue hits or not
                 let matchCCFList = CCF_LIST.hasOwnProperty(venueDBLPURL);
 
-                if (filter.categoryList.length > 0
-                    || filter.rankList.length > 0
-                    || filter.venueList.length > 0) {
+                if (condition.categoryList.length > 0
+                    || condition.rankList.length > 0
+                    || condition.venueList.length > 0) {
                     if (!matchCCFList) {
                         continue;
                     }
 
                     // 根据分类过滤   
-                    if (!isCategoryMatch(venueDBLPURL, filter.categoryList)) {
+                    if (!isCategoryMatch(venueDBLPURL, condition.categoryList)) {
                         continue;
                     }
 
                     // 根据级别过滤
-                    if (!isRankMatch(venueDBLPURL, filter.rankList)) {
+                    if (!isRankMatch(venueDBLPURL, condition.rankList)) {
                         continue;
                     }
 
                     // 根据刊物过滤
-                    if (!isVenueMatch(venueDBLPURL, filter.venueList)) {
+                    if (!isVenueMatch(venueDBLPURL, condition.venueList)) {
                         continue;
                     }
                 }
 
                 // 根据刊物类别过滤
-                if (!isTypeMatch(venueDBLPURL, filter.typeList)) {
+                if (!isTypeMatch(venueDBLPURL, condition.typeList)) {
                     continue;
                 }
 
                 // 根据年份过滤
-                if (!isYearRange(year, filter.year.start, filter.year.end)) {
+                if (!isYearRange(year, condition.year.start, condition.year.end)) {
                     continue;
                 }
 
@@ -156,6 +156,10 @@ function doSearch(query, firstHit, pageSize, total, paperList, filter, fillPaper
                     url = "javascript:void(0)";
                 }
 
+                // 一篇论文可能会同时出现在正式刊物和arXiv上，加上venueName，可以避免Id重复
+                let abstractId = (title + venueName).replace(/[^a-zA-Z]+(.)/g, function (match, group1) {
+                    return group1.toUpperCase();
+                });
                 paperList.push({
                     ccfRank: ccfRank,
                     title: title,
@@ -164,13 +168,13 @@ function doSearch(query, firstHit, pageSize, total, paperList, filter, fillPaper
                     year: paper["year"],
                     url: url,
                     doi: paper["doi"],
-                    abstractId: title.replace(/[^a-zA-Z]+/g, "_"),
+                    abstractId: abstractId
                 });
             }
 
             // recursion
             if (firstHit + size < total) {
-                doSearch(query, firstHit + pageSize, pageSize, total, paperList, filter, fillPaperListCallback);
+                doQueryPaper(query, firstHit + pageSize, pageSize, total, paperList, condition, searchSuccessHandler, searchFailHandler);
                 return;
             }
         }
@@ -187,181 +191,29 @@ function doSearch(query, firstHit, pageSize, total, paperList, filter, fillPaper
         });
 
         // 执行回调函数，填充论文列表
-        fillPaperListCallback(paperList);
+        searchSuccessHandler(paperList);
     })
         .fail(function (jqXHR, textStatus, errorThrown) {
-            let alertHtml = template.render($("#alertTemplate").html(), {
-                title: "提示信息",
-                message: "请求失败，请重新尝试！"
-            });
-            $("#alert").html(alertHtml);
-            $('#alert').modal();
-
-            // 当请求失败时，不要继续显示加载图片
-            $("#tips").empty();
+            // 执行失败回调函数，显示错误信息
+            searchFailHandler();
         });
 }
 
-function search(fillPaperListCallback) {
-    let invalidClass = "is-invalid";
-    let invalid = false;
-
-    // 验证关键词
-    let $searchFeedback = $(".search-feedback");
-    $(".q").each(function (index) {
-        let $input = $(this);
-
-        let query = $input.val().trim();
-        let isEmpty = query == "";
-        let hasChineseCharacter = query.match(/[\u4E00-\u9FA5]|[\uF900-\uFA2D]/);
-
-        if (isEmpty || hasChineseCharacter) {
-            invalid = true;
-            $input.addClass(invalidClass);
-
-            let errorMsg = isEmpty ? "关键词不能为空！" : "DBLP不支持中文关键词！";
-            $feedback = $searchFeedback.eq(index);
-            $feedback.text(errorMsg);
-        } else {
-            $input.removeClass(invalidClass);
-        }
-    });
-
-    // 验证起止年份
-    let $startYear = $("#startYear");
-    let $endYear = $("#endYear");
-    let startYearStr = $startYear.val().trim();
-    let endYearStr = $endYear.val().trim();
-    let isStartYearEmpty = isEmpty(startYearStr);
-    let isEndYearEmpty = isEmpty(endYearStr);
-
-    function handleYear($year, index) {
-        let currentYear = new Date().getFullYear()
-        let errorMsg = `年份必须位于 (0, ${currentYear}] 区间内！`;
-        let yearStr = $year.val().trim();
-        let yearVal = parseInt(yearStr);
-        let isYearInvalid = !isEmpty(yearStr) && (yearVal <= 0 || yearVal > currentYear);
-        let $yearFeedback = $(".year-feedback").eq(index);
-
-        if (isYearInvalid) {
-            $year.addClass(invalidClass);
-            $yearFeedback.text(errorMsg);
-        } else {
-            $year.removeClass(invalidClass);
-            $yearFeedback.empty();
-        }
-
-        return isYearInvalid;
-    }
-
-    if (!isStartYearEmpty || !isEndYearEmpty) {
-        let isStartYearInvalid = handleYear($startYear, 0);
-        let isEndYearInvalid = handleYear($endYear, 1);
-        if (isStartYearInvalid || isEndYearInvalid) {
-            invalid = true;
-        } else if (!isStartYearEmpty && !isEndYearEmpty
-            && parseInt(startYearStr) > parseInt(endYearStr)) {
-            $startYear.addClass(invalidClass);
-            $endYear.addClass(invalidClass);
-            $(".year-feedback").eq(0).text("开始年份必须≤结束年份！");
-
-            invalid = true;
-        }
-    } else {
-        $startYear.removeClass(invalidClass);
-        $endYear.removeClass(invalidClass);
-        $(".year-feedback").each(function (index) {
-            $(this).empty();
-        });
-    }
-
-    // 如果关键词和起止年份存在问题，则提前返回
-    if (invalid) {
-        return;
-    }
-
+function queryPaper(query, condition, searchSuccessHandler, searchFailHandler) {
     // 显示加载图片，并清空查询结果
     $("#tips").html($("#loadingTipsTemplate").html());
     $("#result").empty();
-
-    // 拼接查询字符串
-    let query = "";
-    $(".q").each(function (index) {
-        let val = $(this).val().trim();
-        if (index == 0) {
-            query += val;
-        } else {
-            let condition = $(".condition").eq(index - 1).val();
-            switch (condition) {
-                case "and":
-                    query += " " + val;
-                    break;
-                case "or":
-                    query += "|" + val;
-                    break;
-            }
-        }
-    });
-    // 判断是否需要显示查询字符串
-    if ($("#queryStringInput").prop("checked")) {
-        $("#queryString").text(query);
-    } else {
-        $("#queryString").empty();
-    }
 
     let firstHit = 0;
     // DBLP允许的最大值为1000
     let pageSize = 1000;
     let total = 0;
     let paperList = [];
-    let filter = {
-        categoryList: $("#category").val(),
-        typeList: $("#type").val(),
-        rankList: $("#rank").val(),
-        venueList: $("#venue").val(),
-        year: {
-            start: startYearStr,
-            end: endYearStr
-        }
-    };
 
-    doSearch(query, firstHit, pageSize, total, paperList, filter, fillPaperListCallback);
+    doQueryPaper(query, firstHit, pageSize, total, paperList, condition, searchSuccessHandler, searchFailHandler);
 }
 
-function queryAbstract(paperDOI, paperTitle = null, abstractSelector) {
-    // read cache if exists
-    let $abstractTag = $(abstractSelector);
-    if ($abstractTag.html()) {
-        if ($abstractTag.css("display") == "block") {
-            $abstractTag.css("display", "none");
-        } else {
-            $abstractTag.css("display", "block");
-        }
-        return;
-    }
-
-    // init
-    let $loadingTips = $(abstractSelector + "tips");
-    $loadingTips.html($("#loadingTipsTemplate").html());
-    let errorMsg = "没找到摘要=_=";
-
-    // query
-    doQueryAbstract(
-        paperDOI,
-        paperTitle,
-        $abstractTag,
-        $loadingTips,
-        errorMsg
-    );
-}
-
-function doQueryAbstract(
-    paperDOI,
-    paperTitle,
-    $abstractTag,
-    $loadingTips,
-    errorMsg
-) {
+function queryAbstract(paperDOI, paperTitle, queryAbstractSuccessHandler, queryAbstractFailHandler) {
     let semanticScholarUrl = "https://api.semanticscholar.org/graph/v1/paper/";
     let inputData = {
         fields: "title,abstract",
@@ -381,38 +233,24 @@ function doQueryAbstract(
         // 如果doi搜索不到，那么按标题搜索
         if (paperDOI.trim().length > 0) {
             if (!data["abstract"]) {
-                doQueryAbstract(
-                    "",
-                    paperTitle,
-                    $abstractTag,
-                    $loadingTips,
-                    errorMsg
-                );
+                queryAbstract("", paperTitle, queryAbstractCallback, failCallback);
                 return;
             }
             abstract = data["abstract"];
         } else {
             if (data["total"] == 0 || !data["data"][0]["abstract"]) {
-                $abstractTag.html(errorMsg);
-                $loadingTips.empty();
-                return;
+                abstract = "";
+            } else {
+                let paper = data["data"][0];
+                abstract = paper["abstract"];
             }
-
-            let paper = data["data"][0];
-            abstract = paper["abstract"];
         }
 
-        $abstractTag.html(abstract);
-        $loadingTips.empty();
+        // 执行成功回调函数，填充论文摘要
+        queryAbstractSuccessHandler(abstract);
     })
         .fail(function (jqXHR, textStatus, errorThrown) {
-            let alertHtml = template.render($("#alertTemplate").html(), {
-                title: "提示信息",
-                message: "请求失败，请重新尝试！"
-            });
-            $("#alert").html(alertHtml);
-            $('#alert').modal();
-
-            $loadingTips.empty();
+            // 执行失败回调函数，显示错误信息
+            queryAbstractFailHandler();
         });
 }
